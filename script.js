ec9dc935e5923ea17d4efe0ccb101175e893dbd9
Daan Mooij <daanmooij@gmail.com>
2024-05-02 23:22:30 +0200

    determine position for dropping

--- script.js
+++ script.js
@@ -14,7 +14,27 @@ draggables.forEach(draggable => {
 containers.forEach(container => {
     container.addEventListener('dragover', event => {
         event.preventDefault()
+        const afterElement = getDragAfterElement(container, event.clientY)
         const draggable = document.querySelector('.dragging')
-        container.appendChild(draggable)
+
+        if (afterElement == null) {
+            container.appendChild(draggable)
+        } else {
+            container.insertBefore(draggable, afterElement)
+        }
     })
 })
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        if (offset < 0 && offset > closest.offset) {
+            return { offset:offset, element: child }
+        } else {
+            return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
